78d8aa10bbac04a907b4d6b65e186fade3feb5e6
Sasa Tunde Femi <35744541+sasquare@users.noreply.github.com>
2025-06-01 06:48:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,13 +12,29 @@ dotenv.config();
 const app = express();
 const server = http.createServer(app);
 
-origin: function (origin, callback) {
-  if (!origin || origin.includes("vercel.app") || allowedOrigins.includes(origin)) {
-    callback(null, true);
-  } else {
-    callback(new Error("Not allowed by CORS"));
-  }
-}
+// CORS middleware
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:3000",
+        "https://fidel-bridge-frontend.vercel.app",
+        "https://fidel-bridge-frontend-kloswasz3-femis-projects-0c9c7b22.vercel.app",
+        "https://fidel-bridge-frontend-9rcubtqqy-femis-projects-0c9c7b22.vercel.app",
+        "https://fidel-bridge-frontend-dxdawwzpb-femis-projects-0c9c7b22.vercel.app",
+        "https://fidel-bridge-frontend-fuftn9cn9-femis-projects-0c9c7b22.vercel.app"
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 
 
 // CORS middleware
